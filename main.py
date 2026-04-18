cb9dffc3b8e91408e0ec458cd0688b6be467bc92
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:23:26 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-son = float(input("Son kiriting: "))
-yarim = son / 2
-print(yarim)
+sonlar = []
+n = int(input("Sonlar sonini kiriting: "))
+for i in range(n):
+    son = float(input("Sonni kiriting: "))
+    sonlar.append(son)
+yigindi = sum(sonlar)
+print("Sonlar yig'indisi:", yigindi)
+print("Sonlar:")
+for i, son in enumerate(sonlar, start=1):
+    print(f"{i}. {son}")
